cb5c745e0a6d950bac2e0b44b652cafe49b8618f
kevllino <kevin.eid3@mail.dcu.ie>
2018-08-17 21:49:57 +0100

    try to fix

--- wanchain.rb
+++ wanchain.rb
@@ -6,7 +6,6 @@ class Wanchain < Formula
   homepage "https://github.com/kevllino/go-wanchain"
   url "https://github.com/wanchain/go-wanchain/archive/v1.0.4.tar.gz"
   version "1.0.4"
-  url "https://github.com/kevllino/go-wanchain.git", :tag => 'v0.1.0'
   sha256 "d3d61fc0509ab5db6ec0e1387048d249b8762489352733b636964ea71a43460c"
 
   devel do
@@ -18,7 +17,7 @@ class Wanchain < Formula
 
   depends_on :xcode => :build
 
-  depends_on 'go' => :build
+  # depends_on 'go' => :build
 
   def install
     ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go@1.10/libexec"
