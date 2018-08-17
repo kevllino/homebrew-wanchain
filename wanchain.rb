8334ab6a3424d5c9c64c2a81b82eff26af68f025
kevllino <kevin.eid3@mail.dcu.ie>
2018-08-17 22:09:13 +0100

    up

--- wanchain.rb
+++ wanchain.rb
@@ -2,10 +2,8 @@
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Wanchain < Formula
-  desc "wanchain block chain main code"
   homepage "https://github.com/kevllino/go-wanchain"
   url 'https://github.com/wanchain/go-wanchain.git', :tag => 'v1.0.4'
-  sha256 "d86806163114698335c33db25a4c09bdf2c8ee00f7cd0d79e1cff0c004c0f814"
 
   devel do
     url 'https://github.com/kevllino/go-wanchain.git', :branch => 'master'
@@ -16,7 +14,7 @@ class Wanchain < Formula
 
   depends_on :xcode => :build
 
-  # depends_on 'go' => :build
+  depends_on 'go' => :build
 
   def install
     ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go@1.10/libexec"
